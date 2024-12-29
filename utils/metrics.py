df2d405b0fa84b54dbad27774d23bf593582a315
Julius Heidmann <164488734+Juxh01@users.noreply.github.com>
2024-12-29 18:15:11 +0100

    Added Zero Division to recall score

--- utils/metrics.py
+++ utils/metrics.py
@@ -57,7 +57,9 @@ class Metrics:
         """
         Calculates the recall score.
         """
-        return metrics.recall_score(y_actual, y_predicted, average="macro")
+        return metrics.recall_score(
+            y_actual, y_predicted, average="macro", zero_division=0
+        )
 
     @staticmethod
     def calculate_auc(
@@ -67,7 +69,10 @@ class Metrics:
         Calculates the AUC score.
         """
         return metrics.roc_auc_score(
-            y_actual, y_predicted, average="macro", multi_class="ovo"
+            y_actual,
+            y_predicted,
+            average="macro",
+            multi_class="ovo",
         )
 
     @staticmethod
